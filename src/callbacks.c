05f9e6b44be439e879c8073e100d95cdac5715a8
Pierre Pronchery <khorben@defora.org>
2013-02-23 18:16:13 +0100

    src/callbacks.c no longer translates anything

--- src/callbacks.c
+++ src/callbacks.c
@@ -21,13 +21,11 @@
 #include <string.h>
 #include <libgen.h>
 #include <errno.h>
-#include <libintl.h>
 #include <Desktop.h>
 #include "../include/Player.h"
 #include "player.h"
 #include "callbacks.h"
 #include "../config.h"
-#define _(string) gettext(string)
 
 
 /* macros */
