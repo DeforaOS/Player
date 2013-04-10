dd9da9bc44dd7b0970cc84137d97a08e0ca7eaa9
Pierre Pronchery <khorben@defora.org>
2013-04-11 01:22:46 +0200

    libDesktop API update

--- src/player.c
+++ src/player.c
@@ -506,8 +506,8 @@ Player * player_new(void)
 	/* mplayer */
 	_player_start(player);
 	/* messages */
-	desktop_message_register(PLAYER_CLIENT_MESSAGE, on_player_message,
-			player);
+	desktop_message_register(player->window, PLAYER_CLIENT_MESSAGE,
+			on_player_message, player);
 	return player;
 }
 
